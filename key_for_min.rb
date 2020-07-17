eee4f1f9f1a275123734a7af4da7271bd1102dd9
Warda Ayaz <wayaz107@gmail.com>
2020-07-17 04:36:17 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,9 +7,8 @@ def key_for_min_value(name_hash)
   name_hash do |key,value|
     if lowest_value == nil || value < lowest_value
       lowest_value = value
-      lowest_key = key 
-    end 
-  end 
+      lowest_key = key
+    end
+  end
   lowest_key
   end
-
